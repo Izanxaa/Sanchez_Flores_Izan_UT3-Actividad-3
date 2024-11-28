b56038ceb891d36a6547d6e582f2c5e257650824
Izan <ifloressan@educacion.navarra.es>
2024-11-28 22:45:27 +0100

    commit comentarios AppPokemon

--- juegoPokemon/src/AppPokemon.java
+++ juegoPokemon/src/AppPokemon.java
@@ -1,3 +1,13 @@
+/**
+ *
+ * La clase AppPokemon inicia el juego creando una instancia de InterfazPokemon y
+ * llamando al método principal Juego.
+ *
+ * @author Izan
+ * @version 1.0
+ * @link Combate
+ */
+
 public class AppPokemon {
 
     public static void main(String[] args) {
